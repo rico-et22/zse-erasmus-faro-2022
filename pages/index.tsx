2c771bd30894ad2d0391987cc9513db0b67a211a
Bartek-Dusza <61563032+Bartek-Soul@users.noreply.github.com>
2022-05-01 21:15:00 +0100

    InfoGraphics

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import TextContentWithImage from '../components/ContentWriting'
 import FooterLogos from '../components/FooterLogos'
 import HeaderBanner from '../components/index/HeaderBanner'
+import InfoGraphics from '../components/infographics'
 import Navbar from '../components/Navbar'
 const Home: NextPage = () => {
   return (
@@ -11,10 +12,9 @@ const Home: NextPage = () => {
         <title>Praktyki Erasmus+ ZSE Faro & Algarve 2022</title>
         <meta name="description" content="Strona podsumowująca praktyki zawodowe Erasmus+ Zespołu Szkół Elektronicznych w Rzeszowie - Faro, Portugalia 2022" />
       </Head>
-
       <Navbar />
-      
       <HeaderBanner />
+
       <FooterLogos />
 
     </div>
