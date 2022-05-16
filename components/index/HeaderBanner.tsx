5b63eb4b8666b25bf60715343b30852479816f5f
Kamil Pawlak <ricoet22@gmail.com>
2022-05-16 21:25:02 +0200

    chore: HeaderBanner change photo

--- components/index/HeaderBanner.tsx
+++ components/index/HeaderBanner.tsx
@@ -6,9 +6,9 @@ import image from "../../public/header-banner.jpg"
 export default function HeaderBanner() {
   return (
     <div className="header-banner relative">
-      <Image src={image} alt="Zdjęcie grupowe" layout="fill" objectFit="cover" objectPosition="75% 68%" />
-      <div className="absolute top-0 right-0 bg-gray-900 bg-opacity-40 w-full h-full flex items-end justify-center sm:justify-end" placeholder="blur">
-        <div className="mb-6 sm:mr-24 flex flex-col items-start">
+      <Image src={image} alt="Zdjęcie grupowe" layout="fill" objectFit="cover" objectPosition="75% 50%" />
+      <div className="absolute top-0 right-0 bg-gray-900 bg-opacity-40 w-full h-full flex items-end justify-center sm:justify-start" placeholder="blur">
+        <div className="mb-6 sm:ml-24 flex flex-col items-start">
           <div className="bg-white py-2 px-4 rounded-t-xl">
             <p className="text-sm lg:text-base">Praktyki Erasmus+ w Portugalii</p>
           </div>
